dcc726b4acef155e698b44e4dfab4dc8886004a9
Osayzin <teyretteb@gmail.com>
2026-03-20 11:31:37 -0300

    desafios da aula 2

---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,7 @@
+nome = input ('Digite seu nome: ')
+
+print (f'Boas-vindas {nome}!')
+
+dia_de_nascimento = input ("Digite sua data de nascimento: ")
+
+print (f"Você nasceu em {dia_de_nascimento}!")
