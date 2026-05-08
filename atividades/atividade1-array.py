ce9584e5fe9b0b58535d43a5b686fefdfdd0b876
osaybtw <teyreteb@gmail.com>
2026-05-08 11:21:17 -0300

    atividade nova

--- atividades/atividade1-array.py
+++ atividades/atividade1-array.py
@@ -3,3 +3,14 @@ duplas = ["Bob", "Luis", "Ana", "Jô"]
 for i in range(len(duplas)):
     for j in range(i+1, len(duplas)):
         print(duplas[i], duplas[j])
+print()
+
+musicas = [
+    ["Hero", "Mili"],
+    ["Color Your Night", "Lotus Juice"],
+    ["Hyervetilation", "Radwimps"]
+]
+
+for song in musicas:
+    for info in song:
+        print(info)
